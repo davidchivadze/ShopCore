b96b7bf99410b644b1e479e139287f80a76c8728
Your Name <datochicho11@gmail.com>
2020-07-13 22:49:53 +0400

    last Commit

--- Models/AdminPanel/ViewModels/Accounting/UserLoginModel.cs
+++ Models/AdminPanel/ViewModels/Accounting/UserLoginModel.cs
@@ -5,11 +5,18 @@ using System.Text;
 
 namespace Models.AdminPanel.ViewModels.Accounting
 {
-    public class UserLoginModel
+    public class UserLoginRequestModel
     {
         [Required]
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
+    }
+    public class UserLoginResponseModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserFullName { get; set; }
+        public string Token { get; set; }
     }
 }
